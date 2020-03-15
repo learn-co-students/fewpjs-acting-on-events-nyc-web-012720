5bc3842505e6d82dfb435c7c8d7a0975a9bd7295
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-15 13:33:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+console.log("it's not so bad");
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.addEventListener("keydown", function(event){
+        if (event.key === "ArrowLeft"){
+            moveDodgerLeft()
+        };
+        if (event.key === "ArrowRight"){
+            moveDodgerRight()
+        }
+    });
+});
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`
+    }
+}
