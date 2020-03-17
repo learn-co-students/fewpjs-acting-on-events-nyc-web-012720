9968b8a1e1d21eccdd4407d2f6c082622d46e504
github username <github email address>
2020-03-17 18:43:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+// Your code heredocument.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+});
+
+function moveDodgerLeft(){
+  let leftNumbers = dodger.style.left.replace("px", "");
+  if (leftNumbers > 0) {
+  let left = parseInt(leftNumbers, 10);
+
+  dodger.style.left = `${left - 1}px`;
+  } else alert("Hit a motherfucking wall")
+}
+function moveDodgerRight(){
+  let leftNumbers = dodger.style.left.replace("px", "");
+  if (leftNumbers < 360){
+    let left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left + 1}px`;
+  }
+  else alert("Hit a motherfucking wall")
+}
